8a103d938d1aa344198ed9592b27e108370b7973
jazminteng <jazminteng@gmail.com>
2019-06-19 02:22:20 -0300

    correccion de comas

--- problema2.py
+++ problema2.py
@@ -8,7 +8,7 @@ for line in suc:
 	mens1[i].pop(2)
 	mens1[i][0]=int(mens1[i][0])
 	i=i+1 
-print (mens1)
+#print (mens1)
 
 art=open('articulos.txt','r')
 mens2=[]
@@ -19,7 +19,7 @@ for line in art:
 	mens2[i][0]=int(mens2[i][0])
 	mens2[i][2]=float(mens2[i][2])
 	i=i+1 
-print (mens2)
+#print (mens2)
 
 axs=open('articulosXsucursal.txt','r')
 mens3=[]
@@ -34,7 +34,7 @@ for line in axs:
 	mens3[i][3]=int(mens3[i][3])
 	i=i+1 
 	aux.append([0,0])
-print (mens3)
+#print (mens3)
 salida=open('Infsal.txt', 'w')
 salida.write("Articulos a reponer \r\n")
 salida.write("CodSuc - CodArt - Nombre Articulo \n")
@@ -47,18 +47,18 @@ for i in range (0,naxs):
 	sucaux=aux[codart][0]
 	if cant==0:
 		if cantaux>5:
-			salida.write("%5i \t %7i \t %s \n" % (codsuc,codart,mens2[codart-1][1]) ) 
+			salida.write("%5i \t %5i \t %s \n" % (codsuc,codart,mens2[codart-1][1]) ) 
 			cantaux=None		
 		else:
 			aux[codart]=[codsuc,cant]
 	elif cant>5:
 		if cantaux==0 and sucaux!=0:
-			salida.write("%5i \t %7i \t %s \n" % (sucaux,codart,mens2[codart-1][1]) ) 
+			salida.write("%5i \t %5i \t %s \n" % (sucaux,codart,mens2[codart-1][1]) ) 
 			cantaux=None
 		else:
 			aux[codart]=[codsuc,cant]
-print ('auxiliar')
-print (aux)
+#print ('auxiliar')
+#print (aux)
  
 salida.close()
 suc.close()
